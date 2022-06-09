879cfb98a1038776c9b2aed1ea62d7df5278ff11
Anastasiia Shilina <a.shilina29@gmail.com>
2022-06-09 14:47:40 +0200

    add tab-fruit

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,22 @@
  window.addEventListener('DOMContentLoaded', ()=>{
-    
+
+    const tabsParent = document.querySelector('.tabheader__items');
     const tabs = document.querySelectorAll('.tabheader__item');
     const tabsContent = document.querySelectorAll('.tabcontent');
-    const tabsParent = document.querySelector('.tabheader__items');
+     
 
    function hideTabContent(){
-       tabsContent.forEach(item =>{
-        //    item.style.display = 'none';
-        item.classList.add('hide');
-        item.classList.remove('show', 'fade');
-       })
-       tabs.forEach(tab =>{
-           tab.classList.remove('tabheader__item_active');
-       })
+    tabsContent.forEach(item =>{
+// item.style.display = 'none';
+item.classList.add('hide');
+item.classList.remove('show', 'fade');
+    })
+tabs.forEach(item =>{
+    item.classList.remove('tabheader__item_active');
+})
+     
    }
+
 
     function showTabContent(i = 0){
         // tabsContent[i].style.display = 'block';
@@ -25,7 +28,6 @@
 
     hideTabContent();
     showTabContent();
-
 
     tabsParent.addEventListener('click', (event)=>{
 
