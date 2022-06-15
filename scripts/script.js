08bc74214b961c4eeb0ac618c698249248e8d59b
Anastasiia Shilina <a.shilina29@gmail.com>
2022-06-15 15:53:31 +0200

    timer

--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,109 @@
- window.addEventListener('DOMContentLoaded', ()=>{
-
-    const tabsParent = document.querySelector('.tabheader__items');
-    const tabs = document.querySelectorAll('.tabheader__item');
-    const tabsContent = document.querySelectorAll('.tabcontent');
-     
-
-   function hideTabContent(){
-    tabsContent.forEach(item =>{
-// item.style.display = 'none';
-item.classList.add('hide');
-item.classList.remove('show', 'fade');
-    })
-tabs.forEach(item =>{
-    item.classList.remove('tabheader__item_active');
-})
-     
-   }
+ window.addEventListener('DOMContentLoaded', () => {
+     /*Tabs */
+     const tabsParent = document.querySelector('.tabheader__items');
+     const tabs = document.querySelectorAll('.tabheader__item');
+     const tabsContent = document.querySelectorAll('.tabcontent');
 
 
-    function showTabContent(i = 0){
-        // tabsContent[i].style.display = 'block';
-        tabsContent[i].classList.add('show', 'fade');
-        tabsContent[i].classList.remove('hide');
-        tabs[i].classList.add('tabheader__item_active');
+     function hideTabContent() {
+         tabsContent.forEach(item => {
+             // item.style.display = 'none';
+             item.classList.add('hide');
+             item.classList.remove('show', 'fade');
+         })
+         tabs.forEach(item => {
+             item.classList.remove('tabheader__item_active');
+         })
 
-    }
+     }
 
-    hideTabContent();
-    showTabContent();
 
-    tabsParent.addEventListener('click', (event)=>{
+     function showTabContent(i = 0) {
+         // tabsContent[i].style.display = 'block';
+         tabsContent[i].classList.add('show', 'fade');
+         tabsContent[i].classList.remove('hide');
+         tabs[i].classList.add('tabheader__item_active');
 
-    const target = event.target;
-    if(target && target.classList.contains('tabheader__item')){
-        tabs.forEach((item, i) =>{
-            if(target == item){
-            hideTabContent();
-            showTabContent(i);
+     }
+
+     hideTabContent();
+     showTabContent();
+
+     tabsParent.addEventListener('click', (event) => {
+
+         const target = event.target;
+         if (target && target.classList.contains('tabheader__item')) {
+             tabs.forEach((item, i) => {
+                 if (target == item) {
+                     hideTabContent();
+                     showTabContent(i);
+                 }
+             })
+         }
+     })
+
+
+     /*Chronometer */
+     const deadLine = '2022-07-01';
+
+     function getTimeRemaining(endTime) {
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endTime) - Date.parse(new Date());
+        if(t <= 0){
+    days = 0;
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24));
+            hours = Math.floor((t / 1000 * 60 * 60) % 24);
+            minutes = Math.floor((t / 1000 / 60) % 60);
+            seconds = Math.floor((t / 1000) % 60);
         }
-    })
-    }
-    })
+
+         return {
+             'total': t,
+             'days': days,
+             'hours': hours,
+             'minutes': minutes,
+             'seconds': seconds
+         };
+
+     }
+
+     function getZero(num) {
+         if (num >= 0 && num < 10) {
+             return `0${num}`;
+         } else {
+             return num;
+         }
+     }
+
+     function setClock(selector, endTime) {
+         const timer = document.querySelector(selector);
+         const days = timer.querySelector('#days');
+         const hours = timer.querySelector('#hours');
+         const minutes = timer.querySelector('#minutes');
+         const seconds = timer.querySelector('#seconds');
+         timeInterval = setInterval(updateClock, 1000);
+
+         updateClock();
+
+         function updateClock() {
+             const t = getTimeRemaining(endTime);
+             days.innerHTML = getZero(t.days);
+             hours.innerHTML = getZero(t.hours);
+             minutes.innerHTML = getZero(t.minutes);
+             seconds.innerHTML = getZero(t.seconds);
+
+             if (t.total <= 0) {
+                 clearInterval(timeInterval);
+             }
+         }
+     }
+
+
+
+     setClock('.timer', deadLine);
 
  })
