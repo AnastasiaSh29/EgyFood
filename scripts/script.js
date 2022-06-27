0550a2b370a07f9f618898aaa21ceea75c828413
Anastasiia Shilina <a.shilina29@gmail.com>
2022-06-27 07:49:02 +0200

    LS for forms

--- scripts/script.js
+++ scripts/script.js
@@ -110,7 +110,7 @@
      /*Modal */
      const modalTrigger = document.querySelectorAll('[data-modal]');
      const modal = document.querySelector('.modal');
-     const modalCloseBtn = document.querySelector('[data-close');
+    //  const modalCloseBtn = document.querySelector('[data-close');
      const modalTimerId = setTimeout(openModal, 5000);
 
 
@@ -136,10 +136,10 @@
          btn.addEventListener('click', openModal)
      })
 
-     modalCloseBtn.addEventListener('click', closeModal);
+    //  modalCloseBtn.addEventListener('click', closeModal);
 
      modal.addEventListener('click', (e) => {
-         if (e.target === modal) {
+         if (e.target === modal || e.target.getAttribute('data-close') == '') {
              closeModal();
          }
      })
@@ -152,6 +152,82 @@
 
      window.addEventListener('scroll', showModalByScroll);
 
+     /*forms*/
+     const forms = document.querySelectorAll('form');
+     const message ={
+        // loading: ' Загрузка',
+        loading: 'images/form/spinner.svg',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: ' Что-то пошло не так ...'
+     }
+
+     forms.forEach(item =>{
+        postData(item);
+     })
+
+     function postData(form){
+        form.addEventListener('submit', (e)=>{
+            e.preventDefault();
+
+            const statusMessage = document.createElement('img');
+            statusMessage.src = message.loading;
+               statusMessage.style.cssText = `
+               display: block;
+               margin: 0 auto; 
+               `;
+            form.insertAdjacentElement('afterend', statusMessage);
+
+            
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(value,key){
+                object[key] = value;
+            });
+
+            fetch('server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            }).then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
+            })
+        });
+     }
+
+     function showThanksModal(message){
+        const prevModalDialog = document.querySelector('.modal__window');
+
+        prevModalDialog.classList.add('hide');
+        openModal();
+
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('modal__window');
+        thanksModal.innerHTML = `
+        <div class='modal__content'>
+        <div data-close class="modal__close">&times;</div>
+        <div class="modal__title">${message}</div>
+        </div>
+        `;
+
+        document.querySelector('.modal').append(thanksModal);
+        setTimeout(()=>{
+            thanksModal.remove();
+            prevModalDialog.classList.add('show');
+            prevModalDialog.classList.remove('hide');
+            closeModal();
+        }, 4000);
+     }
 
      /*cards */ 
 
@@ -243,7 +319,6 @@
                     const data = JSON.parse(request.response); //распарсили ответ
                     inputUsd.value =(+inputRub.value / data.current.usd).toFixed(2);
                     inputLe.value =(+inputRub.value / data.current.le).toFixed(2);
-
 
 
                 }else{
